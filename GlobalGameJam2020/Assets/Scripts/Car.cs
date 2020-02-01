caab6a68374dbb2c8cadf777fc5ab2f058048857
Fabricio Brito <fabricio.brito@gmail.com>
2020-02-01 04:28:16 -0300

    Basic Score system by car based on seconds moving or stopped

--- GlobalGameJam2020/Assets/Scripts/Car.cs
+++ GlobalGameJam2020/Assets/Scripts/Car.cs
@@ -11,6 +11,13 @@ public class Car : MonoBehaviour
     public bool isHorizontal = true;
 
     private float speed;
+
+    public float score = 1.0f;
+    public float secondsStoppedToDowngrade = 5.0f;
+    public float secondsMovingToUpgrade = 10.0f;
+    public float scoreStepSize = .2f;
+    public float timeStopped = 0.0f;
+    public float timeMoving = 0.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,10 +28,36 @@ public class Car : MonoBehaviour
 // Update is called once per frame
 void Update()
     {
+        float prevSpeed = speed;
         speed += acceleration;
         speed = Mathf.Min(speed , maxSpeed);
         speed = Mathf.Max(speed, 0);
 
+        // Check Stopped/Moving time
+        if(prevSpeed == 0 && speed == 0)
+        {
+            timeStopped += Time.deltaTime;
+            timeMoving = 0;
+        }
+        if (prevSpeed > 0 && speed > 0)
+        {
+            timeMoving += Time.deltaTime;
+            timeStopped = 0;
+        }
+        
+        // Check If stopped/moved for time enough to change score
+        if(timeStopped > secondsStoppedToDowngrade)
+        {
+            score = Mathf.Max(0, score - scoreStepSize);
+            timeStopped = 0;
+        }
+        if(timeMoving > secondsMovingToUpgrade)
+        {
+            score = Mathf.Min(score + scoreStepSize, 1.0f);
+            timeMoving = 0;
+        }
+        
+        // Actually moves the car
         if (isHorizontal)
         {
             this.transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
@@ -34,14 +67,6 @@ void Update()
             this.transform.position += new Vector3(0, -speed*Time.deltaTime, 0);
         }
     }
-
-    //void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.CompareTag("trafficLight"))
-    //    {
-    //        speed = 0f;
-    //    }
-    //}
 
     private void OnTriggerStay2D(Collider2D other)
     {
