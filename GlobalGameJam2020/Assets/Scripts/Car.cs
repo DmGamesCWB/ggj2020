a7434f9d7aa41b63ba7cc39c908bb9edac599be2
Fabricio Brito <fabricio.brito@gmail.com>
2020-02-01 03:22:08 -0300

    Acceleration and deacceleration logic

--- GlobalGameJam2020/Assets/Scripts/Car.cs
+++ GlobalGameJam2020/Assets/Scripts/Car.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    public float initialSpeed = 15.0f;
+    // Physics Variables
+    public float maxSpeed = 15.0f;
+    public float acceleration = 0.5f;
+    
     public bool isHorizontal = true;
 
     private float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        speed = initialSpeed;
+        speed = 0;
     }
 
 // Update is called once per frame
 void Update()
     {
+        speed += acceleration;
+        speed = Mathf.Min(speed , maxSpeed);
+        speed = Mathf.Max(speed, 0);
+
         if (isHorizontal)
         {
             this.transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
@@ -27,17 +35,31 @@ void Update()
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    //void OnCollisionEnter2D
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("trafficLight"))
+    //    {
+    //        speed = 0f;
+    //    }
+    //}
+
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("trafficLight"))
         {
-            speed = 0f;
+            if (acceleration > 0)
+            {
+                acceleration = -acceleration;
+            }
         }
+
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        speed = initialSpeed;
+        if (acceleration < 0)
+        {
+            acceleration = -acceleration;
+        }
     }
 }
