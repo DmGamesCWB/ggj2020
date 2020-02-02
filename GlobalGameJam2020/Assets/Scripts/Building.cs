f4ae04345ae4d175cf17685ec521261a85782209
fsrechia <feliperechia@gmail.com>
2020-02-02 01:36:23 -0300

    Add repairable prefab house
    - Resize derelicthouse emoji to 160px;
    - Create building script to use on repairable buildings;
    
    Signed-off-by: fsrechia <feliperechia@gmail.com>

---
+++ GlobalGameJam2020/Assets/Scripts/Building.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool isDamaged = true;
+    public Sprite[] buildingSprites;
+    private int spriteIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateBuildingState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if MouseClick occurred while hovering
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            // If a gameObject collides with the Raycast in MousePosition
+            if (hit.collider.gameObject.name == this.name)
+            {
+                isDamaged = false;
+                UpdateBuildingState();
+            }
+        }
+    }
+
+    void UpdateBuildingState()
+    {
+        spriteIndex = isDamaged ? 1 : 0;
+        transform.GetComponent<SpriteRenderer>().sprite = buildingSprites[spriteIndex];
+    }
+}
